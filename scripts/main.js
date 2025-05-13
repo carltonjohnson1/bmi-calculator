06d4dede614f080cedb3579bcea0276fdfeb50ca
carltonjohnson1 <carltonj1@hotmail.com>
2025-05-13 11:06:37 +0100

    update to all files and making it mobile responsive

--- scripts/main.js
+++ scripts/main.js
@@ -10,18 +10,19 @@ function metricDoCalculations() {
     document.getElementById("metricGuideHeading").innerHTML = `Your BMI is: ${metricBMI.toFixed(2)}`;
 
     if (metricBMI < 18.5) {
-        document.getElementById("metricBmiResultmessage").innerHTML = "Classed as Under Ideal Weight";
+        document.getElementById("metricBmiResultmessage").innerHTML = "Your BMI suggests you are in the underweight range category.";
         updateBMIImage("underweight");
     } else if (metricBMI >= 18.5 && metricBMI <= 24.9) {
-        document.getElementById("metricBmiResultmessage").innerHTML = "Classed as Healthy Weight";
+        document.getElementById("metricBmiResultmessage").innerHTML = "Your BMI suggests you are in the healthy weight range category.";
         updateBMIImage("healthy");
+        blaskConfetti();
 
     }  else if (metricBMI >= 25 && metricBMI <= 29.9) {
-        document.getElementById("metricBmiResultmessage").innerHTML = "Classed as Over Ideal Weight";
+        document.getElementById("metricBmiResultmessage").innerHTML = "Your BMI suggests you are in the overweight range category.";
         updateBMIImage("overweight");
     } 
     else {
-        document.getElementById("metricBmiResultmessage").innerHTML = "Classed as High-Risk Category (Obese)";
+        document.getElementById("metricBmiResultmessage").innerHTML = "Your BMI suggests you are in the obese range category.";
         updateBMIImage("obese");
     }
 
@@ -38,20 +39,22 @@ function imperialDoCalculations() {
     document.getElementById("imperialGuideHeading").innerHTML = `Your BMI is: ${imperialBMI.toFixed(2)}`;
 
     if (imperialBMI < 18.5) {
-        document.getElementById("imperialBmiResultmessage").innerHTML = "Classed as Under Ideal Weight";
+        document.getElementById("imperialBmiResultmessage").innerHTML = "Your BMI suggests you are in the underweight range category.";
         updateBMIImage("underweight");
     } else if (imperialBMI >= 18.5 && imperialBMI <= 24.9) {
-        document.getElementById("imperialBmiResultmessage").innerHTML = "Classed as Healthy Weight";
+        document.getElementById("imperialBmiResultmessage").innerHTML = "Your BMI suggests you are in the healthy weight range category.";
         updateBMIImage("healthy");
+        blaskConfetti();
+        
 
     } else if (imperialBMI >= 25 && imperialBMI <= 29.9) {
-        document.getElementById("imperialBmiResultmessage").innerHTML = "Classed as High-Risk Category (Obese)";
+        document.getElementById("imperialBmiResultmessage").innerHTML = "Your BMI suggests you are in the overweight range category.";
          updateBMIImage("overweight");
 
     }
 
     else {
-        document.getElementById("imperialBmiResultmessage").innerHTML = "You are obese";
+        document.getElementById("imperialBmiResultmessage").innerHTML = "Your BMI suggests you are in the obese range category.";
         updateBMIImage("obese");
     }
 
@@ -136,4 +139,49 @@ function updateBMIImage(category) {
 
 function weightRanges() {
     document.getElementById("weightRanges").style.display = "block";
+}
+
+//animated confetti
+
+const blaskConfetti = ()=> {
+
+const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
 }
